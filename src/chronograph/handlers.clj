866c942e4eb535611fa65867a657947717fe274d
Joe Wass <joe@afandian.com>
2015-01-06 19:23:18 +0000

    Added manual list of interesting DOIs, wording on homepage.

--- src/chronograph/handlers.clj
+++ src/chronograph/handlers.clj
@@ -76,11 +76,67 @@
               (export-info (::info ctx))
               (::info ctx)))
 
+; A quick list manually hacked
+; select distinct(events.doi), length(event_timelines.timeline) as e  from event_timelines inner join events on events.doi = event_timelines.doi where events.event > "2013-01-01" order by e desc limit 50;
+(def interesting-dois [
+"10.1007/s12098-012-0960-0"     
+"10.1007/s12098-012-0869-7"     
+"10.1007/s12098-012-0945-z"     
+"10.1111/cmi.12155"             
+"10.1111/cmi.12154"             
+"10.1007/s12098-012-0924-4"     
+"10.1111/ddg.12073"             
+"10.1080/00986445.2012.746673"  
+"10.1111/bij.12071"             
+"10.1007/s12098-013-1031-x"     
+"10.3892/or.2013.2512"          
+"10.1111/bij.12072"             
+"10.1556/jba.2.2013.003"        
+"10.1111/bij.12066"             
+"10.1007/s12098-012-0883-9"     
+"10.1103/physrevb.87.241403"    
+"10.1007/s12098-013-0968-0"     
+"10.1088/0031-8949/88/01/015402"
+"10.1111/ddg.12009"             
+"10.1111/dmcn.12156"            
+"10.1542/peds.2013-1099"        
+"10.1016/j.irle.2012.11.004"    
+"10.1007/s12098-012-0943-1"     
+"10.1556/jba.2.2013.002"        
+"10.1080/00927872.2012.661005"  
+"10.3892/ijmm.2013.1418"        
+"10.1111/cobi.12079"            
+"10.1111/cp.12008"              
+"10.1111/ddg.12088"             
+"10.1542/peds.2013-0943"        
+"10.1111/bdi.12084"             
+"10.1542/peds.2013-0940"        
+"10.1111/cpsp.12032"            
+"10.1111/bdi.12083"             
+"10.1111/bij.12074"             
+"10.1111/bij.12082"             
+"10.3892/or.2013.2510"          
+"10.1542/peds.2013-1056"        
+"10.1080/01402382.2012.749652"  
+"10.1111/birt.12041"            
+"10.1111/cobi.12085"            
+"10.1103/physrevb.87.214510"    
+"10.1103/physrevb.87.245110"    
+"10.1103/physrevb.87.245418"    
+"10.1016/j.irle.2012.10.005"    
+"10.1088/0031-8949/88/01/015704"
+"10.1080/01402382.2013.783353"  
+"10.1111/bdi.12078"             
+"10.1007/s12098-013-1078-8"     
+"10.1111/cobi.12084"            
+])
+
+
 (defresource home
   []
   :available-media-types ["text/html"]
   :handle-ok (fn [ctx]
-   (render-file "templates/index.html" {})))
+   (render-file "templates/index.html" {:interesting-dois interesting-dois})))
 
 (defresource dois-redirect
   []
@@ -133,7 +189,8 @@
                                      :milestone-events-by-type milestone-events-by-type
                                      :facts facts
                                      :facts-by-type facts-by-type
-                                     :timelines interpolated-timelines}]
+                                     :timelines interpolated-timelines
+                                     }]
                (render-file "templates/doi.html" render-context))))
 
 (defresource domains-redirect
