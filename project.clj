b8c4f8100707b2de1f6400e502dd3391c92a27c0
Joe Wass <joe@afandian.com>
2015-01-22 17:07:42 +0000

    Bump to new CrossRef utils version, more robust DOI input format.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                  [crossref-util "0.1.6"]
+                  [crossref-util "0.1.7"]
                   [clj-http "1.0.1"]
                   [clj-time "0.8.0"]
                   [org.clojure/data.json "0.2.5"]
